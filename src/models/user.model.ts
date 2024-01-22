c1f8dab461be5eb44828aea48b63d3a73ad385a6
OceanOwl <115832268+OceanOwl@users.noreply.github.com>
2024-01-23 00:23:30 +0100

    Initial commit

---
+++ src/models/user.model.ts
@@ -0,0 +1,34 @@
+import { model, Schema } from "mongoose";
+
+import { IUser } from "../types/user.type";
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      lowercase: true,
+      required: true,
+    },
+    age: {
+      type: Number,
+      min: 1,
+      max: 55,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
+
+export const User = model<IUser>("user", userSchema);
